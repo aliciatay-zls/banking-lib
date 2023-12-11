b39d15b2a33353e970ae9e49bca4f5e6d38bfd4a
aliciatay-zls <e0424736@u.nus.edu>
2023-12-12 03:18:04 +0800

    Add custom time package

---
+++ clock/clock.go
@@ -0,0 +1,33 @@
+package clock
+
+import "time"
+
+const FormatDateTime = "2006-01-02 15:04:05" //from time.RFC3339, modified based on banking.sql
+
+type Clock interface {
+	Now() time.Time
+	NowAsString() string
+}
+
+type RealClock struct {
+}
+
+func (c RealClock) Now() time.Time {
+	return time.Now()
+}
+
+func (c RealClock) NowAsString() string {
+	return c.Now().Format(FormatDateTime)
+}
+
+type StaticClock struct {
+}
+
+func (c StaticClock) Now() time.Time {
+	staticDate, _ := time.Parse(FormatDateTime, FormatDateTime)
+	return staticDate
+}
+
+func (c StaticClock) NowAsString() string {
+	return c.Now().Format(FormatDateTime)
+}
