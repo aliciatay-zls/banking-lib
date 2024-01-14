6d4e0d492df0e9f1daf7153829f71aa8a3b32ae9
aliciatay-zls <e0424736@u.nus.edu>
2024-01-15 00:43:57 +0800

    Add custom form validator package

---
+++ formValidator/formValidator.go
@@ -0,0 +1,77 @@
+package formValidator
+
+import (
+	"bufio"
+	"errors"
+	"github.com/go-playground/validator/v10"
+	"os"
+	"regexp"
+)
+
+const UsernameAlias = "un"
+
+var appValidator *validator.Validate
+var codeCountryMap map[string]string
+
+// Create initializes a global validator that uses a custom username validator, as well as a global map of
+// countries' codes to their names.
+func Create() {
+	appValidator = validator.New(validator.WithRequiredStructEnabled())
+	useCustomUsernameValidator()
+	createCodeCountryMap()
+}
+
+// useCustomUsernameValidator registers a custom validator for a username field that needs to be filled,
+// starts with alphabet, only alphanumeric and underscore allowed, min 6 chars and max 20 chars long.
+func useCustomUsernameValidator() {
+	unRegex := regexp.MustCompile("^[A-Za-z]\\w{5,19}$")
+
+	if err := appValidator.RegisterValidation("validusername", func(fl validator.FieldLevel) bool {
+		return unRegex.MatchString(fl.Field().String())
+	}); err != nil {
+		panic(err)
+	}
+
+	appValidator.RegisterAlias(UsernameAlias, "required,min=6,max=20,validusername")
+}
+
+// createCodeCountryMap uses the country code and name pairs provided by the ISO 3166-1 alpha-2 standard.
+// Retrieved 8/1/2024: https://en.wikipedia.org/wiki/ISO_3166-1_alpha-2#Officially_assigned_code_elements
+func createCodeCountryMap() {
+	codeCountryMap = map[string]string{}
+
+	f1, err := os.Open("./formValidator/codes.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f1.Close()
+
+	f2, err := os.Open("./formValidator/countries.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer f2.Close()
+
+	s1 := bufio.NewScanner(f1)
+	s2 := bufio.NewScanner(f2)
+	for s1.Scan() {
+		s2.Scan()
+		codeCountryMap[s1.Text()] = s2.Text()
+	}
+}
+
+func GetCountryFrom(code string) string {
+	return codeCountryMap[code]
+}
+
+// Wrapped functions from go-playground/validator:
+
+func Struct(s interface{}) validator.ValidationErrors {
+	errs := appValidator.Struct(s)
+	if errs != nil {
+		var errsArr validator.ValidationErrors
+		errors.As(errs, &errsArr)
+		return errsArr
+	}
+	return nil
+}
--- go.mod
+++ go.mod
@@ -3,6 +3,18 @@ module github.com/udemy-go-1/banking-lib
 go 1.20
 
 require (
+	github.com/go-playground/validator/v10 v10.17.0
+	go.uber.org/zap v1.25.0
+)
+
+require (
+	github.com/gabriel-vasile/mimetype v1.4.2 // indirect
+	github.com/go-playground/locales v0.14.1 // indirect
+	github.com/go-playground/universal-translator v0.18.1 // indirect
+	github.com/leodido/go-urn v1.2.4 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
-	go.uber.org/zap v1.25.0 // indirect
+	golang.org/x/crypto v0.18.0 // indirect
+	golang.org/x/net v0.20.0 // indirect
+	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/text v0.14.0 // indirect
 )
